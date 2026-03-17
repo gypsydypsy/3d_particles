fe38d9c02276200453576c99c844fc9d10c7a728
Léa Marie <32674670+gypsydypsy@users.noreply.github.com>
2026-03-17 10:48:20 +0100

    add nvmrc + update prod config

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,6 +1,5 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const path = require('path');
 
@@ -23,11 +22,6 @@ module.exports = {
             filename: "[name].css",
             chunkFilename: "[id].css",
         }), 
-        new CopyWebpackPlugin({
-            patterns: [
-                { from: path.resolve(__dirname, '../static'), to: path.resolve(__dirname, '../dist/assets')}
-            ]
-        })
     ],
     module: {
         rules : [
--- config/webpack.prod.js
+++ config/webpack.prod.js
@@ -1,9 +1,17 @@
 const path = require('path')
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = merge(common,
     {
-        mode: 'production'
+        mode: 'production',
+        plugins: [
+            new CopyWebpackPlugin({
+                patterns: [
+                    { from: path.resolve(__dirname, '../static'), to: path.resolve(__dirname, '../dist') }
+                ]
+            })
+        ]
     }
 )
